d2c35c5c7e6a916a90cb89b062ab0cb4f0486871
aten040791 <aten040791@gmail.com>
2024-07-02 16:05:51 +0700

    First commit

--- kernels/validations/index.js
+++ kernels/validations/index.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-const { ExpressValidator } = require('express-validator');
-const response = require('utils/responseUtils');
-
-const { validationResult } = new ExpressValidator({}, {}, {
-    errorFormatter: error => ({
-        field: error.path,
-        message: error.msg
-    })
-});
-
-
-const validate = validationArray => {
-    return async (req, res, next) => {
-        for (let validation of validationArray) {
-            for (let _validation of validation) {
-                await _validation.get().run(req);
-            }
-        }
-
-        const errors = validationResult(req);
-        if (errors.isEmpty()) {
-            return next();
-        }
-
-        return response.invalidated(res, {
-            errors: errors.array()
-        })
-    };
-};
-
-
-module.exports = {validate}
-=======
 const { ExpressValidator } = require("express-validator");
 const response = require("utils/responseUtils");
 
@@ -66,4 +32,3 @@ const validate = (validationArray) => {
 };
 
 module.exports = { validate };
->>>>>>> 2ee19d24445ae4ae76781d0159a880d30eec73b9
